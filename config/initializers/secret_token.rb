51b450d9d8c48de5c1baa69135cc4afac2992af4
Kerri Miller <kerrizor@kerrizor.com>
2014-02-25 11:35:49 -0800

    initial Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HitsOfTheAdies::Application.config.secret_key_base = '14a735ccd88de7014ff9fbbd35aaec6f85d075a57810157f0843eb44469f1a02d2962a5c8895a0775b8a2aaac920e2642ac0608153e70efd125cc91fe3a7e25f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HitsOfTheAdies::Application.config.session_store :cookie_store, key: '_hits_of_the_adies_session'
